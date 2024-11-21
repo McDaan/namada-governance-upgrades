cb65715eed7de83a713930539b3a6c18d8c7ce7b
Mandragora <dan.investor.1996@gmail.com>
2024-11-21 10:48:10 +0100

    Update lib.rs

--- ibcratelimits/src/lib.rs
+++ ibcratelimits/src/lib.rs
@@ -1,4 +1,3 @@
-use dec::Dec;
 use namada_tx_prelude::*;
 
 pub type MintTokenLimit = token::Amount;
@@ -7,14 +6,16 @@ pub type ThroughtputTokenLimit = token::Amount;
 #[transaction]
 fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
   
-    // Enable IBC deposit/withdraws limits for NAM
-    let ibc_denom = format!("transfer/{channel_id}/{base_token}");
-    let token_address = ibc::ibc_token(&ibc_denom).clone();
+    // Enable IBC deposit/withdraws limits for native token NAM
+    let native_token = tx_host_env::with(|tx_env| {
+       let native_token = tx_env.state.in_mem().native_token.clone();
+       native_token
+    });
 
-    let mint_limit_token_key = ibc::mint_limit_key(&token_address);
+    let mint_limit_token_key = ibc::mint_limit_key(&native_token);
     ctx.write(&mint_limit_token_key, MintTokenLimit::from_u64(50000000000))?;
 
-    let throughput_limit_token_key = ibc::throughput_limit_key(&token_address);
+    let throughput_limit_token_key = ibc::throughput_limit_key(&native_token);
     ctx.write(&throughput_limit_token_key, ThroughtputTokenLimit::from_u64(50000000000))?;
 
     Ok(())
