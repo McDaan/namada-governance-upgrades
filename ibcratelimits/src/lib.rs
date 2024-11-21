c66060040af543f89900ecb4f321953c81de84e8
Mandragora <dan.investor.1996@gmail.com>
2024-11-21 10:29:39 +0100

    Update lib.rs

--- ibcratelimits/src/lib.rs
+++ ibcratelimits/src/lib.rs
@@ -1,1 +1,21 @@
-.
+use dec::Dec;
+use namada_tx_prelude::*;
+
+pub type MintTokenLimit = token::Amount;
+pub type ThroughtputTokenLimit = token::Amount;
+
+#[transaction]
+fn apply_tx(ctx: &mut Ctx, _tx_data: BatchedTx) -> TxResult {
+  
+    // Enable IBC deposit/withdraws limits for NAM
+    let ibc_denom = format!("transfer/{channel_id}/{base_token}");
+    let token_address = ibc::ibc_token(&ibc_denom).clone();
+
+    let mint_limit_token_key = ibc::mint_limit_key(&token_address);
+    ctx.write(&mint_limit_token_key, MintTokenLimit::from_u64(50000000000))?;
+
+    let throughput_limit_token_key = ibc::throughput_limit_key(&token_address);
+    ctx.write(&throughput_limit_token_key, ThroughtputTokenLimit::from_u64(50000000000))?;
+
+    Ok(())
+}
